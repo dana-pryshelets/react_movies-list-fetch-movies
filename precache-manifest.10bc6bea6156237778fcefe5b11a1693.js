27bb4f956adceff0cf81d6390dde55ad4277b730
Dana Pryshelets <tanyarud53@gmail.com>
2021-08-07 19:08:48 +0300

    Updates

---
+++ precache-manifest.10bc6bea6156237778fcefe5b11a1693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2549076efa90442426b4bdd321a32645",
+    "url": "./index.html"
+  },
+  {
+    "revision": "062b7f414f56c23c76fe",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "edcac14a3a7b2f4fc64c",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "062b7f414f56c23c76fe",
+    "url": "./static/js/2.6d4a844e.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.6d4a844e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edcac14a3a7b2f4fc64c",
+    "url": "./static/js/main.57d1d2ee.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
